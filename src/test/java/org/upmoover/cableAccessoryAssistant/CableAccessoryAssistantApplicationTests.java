1f3fd25a704917a1396b7e6ed0f844659b51f6c4
upmoover <upmoover@gmail.com>
2020-11-27 22:08:31 +0300

    Init commit

---
+++ src/test/java/org/upmoover/cableAccessoryAssistant/CableAccessoryAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upmoover.cableAccessoryAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CableAccessoryAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
